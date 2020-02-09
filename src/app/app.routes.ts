0136000e41566f2151853a511bff72a7c83ed6bc
Anartz Mugika Ledo <mugan86@gmail.com>
2020-02-09 19:05:43 +0100

    Adaptado a la versión final lazy loading (9)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { ModuleWithProviders } from '@angular/compiler/src/core';
 const APP_ROUTES: Routes = [
     { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)},
+    // { path: 'home', loadChildren: './pages/home/home.module#HomeModule'},
     { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutModule) },
     { path: 'post', loadChildren: () => import('./pages/post/post.module').then(m => m.PostModule) },
     { path: 'portfolio', loadChildren: () => import('./pages/portfolio/portfolio.module').then(m => m.PortfolioModule) },
